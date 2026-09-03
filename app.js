b9442effefc6a084f3200778fc08fcc78e1e7b1a
toto.modena94 <295842057+totomodena94@users.noreply.github.com>
2026-09-03 18:25:05 +0200

    Add player leveling, healer event, and balance rework

--- app.js
+++ app.js
@@ -1,21 +1,58 @@
+const nameEnemy = ["Goblin", "Rata gigante", "Bandido", "Lobo salvaje", "Esqueleto", "Orco", "Arpía", "Trol", "Caballero caído", "Dragón"];
+
 const player = {
     name: "Talos",
     level: 1,
-    pointsLife: 50,
+    pointsLife: 70,
+    maxPointsLife: 50,
     pointsAttack: 10,
     equipment: {
         weapon:"Shortsword",
         shield: "Woodshield"
                 } 
 };
+let combateTerminado = false;
 
-const goblin = {
-    name: "Goblin",
-    pointsLife: 30,
-    pointsAttack: 7,
+for (let round = 1; round <= 10; round++){
+    const enemy = {
+    name: nameEnemy[round -1],
+    pointsLife: 30 + (round * round * 6),
+    pointsAttack: 4 + (round * round),
+    
+};
+    player.level = player.level + 1;
+        let porcentaje = player.pointsLife / player.maxPointsLife
+        player.maxPointsLife = 20 + (round * round *5);
+        player.pointsAttack = 10 + (round *round*2);
+       player.pointsLife = porcentaje * player.maxPointsLife; 
+       console.log(`${player.name} ha subido de nivel. Sus estadísticas se han modificado. Su ataque ahora es de ${player.pointsAttack} y su vida es de ${player.pointsLife}`);
+    
+     if(round % 2 ===0) {
+        if(player.pointsLife < player.maxPointsLifepointsLife * 0.75){
+            player.pointsLife = maxPointsLife * 0.75
+            console.log(`Los dioses reconocen tu coraje y te bendicen. ${player.name} ha recuperado salud)`);
+        };
+        
+     }
+    
+    
+    combateTerminado = false;
+    while (!combateTerminado){
+        attack(player, enemy);
+        attack(enemy, player);
+    }
+
+    if(player.pointsLife <= 0){
+        console.log("Game over");
+        break 
+    }
 };
 
-let combateTerminado = false;
+
+
+
+
+
 
 function attack (atacante, defensor){
     if(combateTerminado){
@@ -30,10 +67,7 @@ function attack (atacante, defensor){
     }
        
 };
-while (!combateTerminado){
-        attack(player, goblin);
-        attack(goblin, player);
-    }
 
-attack(player, goblin);
-attack(goblin, player);
+
+
+
